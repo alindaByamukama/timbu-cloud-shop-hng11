24d5d259c20ed09ad940b3be1073149ab35868c2
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-04 16:15:31 +0300

    add initial product card component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -13,7 +13,24 @@ const ProductInfo = styled.div`
     @apply mt-4;
 `;
 
+const ProductName = styled.h2`
+    @apply text-lg font-bold;
+`;
+
 const ProductDescription = styled.p`
     @apply text-gray-500;
 `;
 
+const ProductCard: React.FC<{ name: String; description: String; imageUrl: string }> = ({ name, description, imageUrl }) => {
+    return (
+        <Card>
+            <ProductImage src={imageUrl} alt={name} />
+            <ProductInfo>
+                <ProductName>{name}</ProductName>
+                <ProductDescription>{description}</ProductDescription>
+            </ProductInfo>
+        </Card>
+    );
+};
+
+export default ProductCard;
