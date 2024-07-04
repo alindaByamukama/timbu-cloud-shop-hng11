c25cb01a817febf3b4a872bddf29ac4f8afa8988
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-04 18:03:22 +0300

    add initial checkout page w/o process

---
+++ pages/checkout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CheckoutPage: React.FC = () => {
+    return (
+        <div className="container mx-auto px-4">
+            <h1 className="text-3xl font-bold my-4">Checkout</h1>
+            <p>Checkout process here</p>
+        </div>
+    );
+};
+
+export default CheckoutPage;
