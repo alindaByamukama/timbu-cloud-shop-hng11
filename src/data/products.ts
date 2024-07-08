081be87b219b6f40fb02d11467a0ea4ed7bd7b82
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-08 17:48:46 +0300

    add product ids

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,11 +1,13 @@
 export const products = [
     {
+        id: 'bamboo-lid',
         imageUrl: '/images/products/bamboo-lid.png',
         name: 'Bamboo Lid',
         description: 'Durable bottle with a sustainable bamboo lid.',
         price: '₦20,000',
     },
     {
+        id: 'insulated',
         imageUrl: '/images/products/insulated.png',
         name: 'Insulated',
         description: 'Keep your drinks hot or cold for hours.',
@@ -18,30 +20,35 @@ export const products = [
         price: '₦15,000',
     },
     {
+        id: 'straw-lid',
         imageUrl: '/images/products/straw-lid.png',
         name: 'Straw Lid',
         description: 'Easy to drink straw lid, perfect for kids',
         price: '₦25,000',
     },
     {
+        id: 'wide-mouth',
         imageUrl: '/images/products/wide-mouth.png',
         name: 'Wide Mouth',
         description: 'Wide mouth for easy filling and cleaning,',
         price: '₦22,000',
     },
     {
+        id: 'glass',
         imageUrl: '/images/products/glass.png',
         name: 'Glass',
         description: 'Elegant glass bottle for everyday use.',
         price: '₦27,000',
     },
     {
+        id: 'collapsible',
         imageUrl: '/images/products/collapsible.png',
         name: 'Collapsible',
         description: 'Space-saving collapsible cup for travel.',
         price: '₦25,000',
     },
     {
+        id: 'infuser',
         imageUrl: '/images/products/infuser.png',
         name: 'Infuser',
         description: 'Classy infuser bottle for hydration.',
