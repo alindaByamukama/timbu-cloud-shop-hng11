2740890d897d080e95d7a6f21941f8a8beb1a220
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-08 21:33:24 +0300

    add header component

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+const Header: React.FC = () => {
+  return (
+    <div className={styles.header}>
+      <div className="logo">
+        <img src="/logo.svg" alt="Logo" />
+        <span>VistaMart</span>
+      </div>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">Products</a>
+        <a href="#">Contact</a>
+      </nav>
+      <div className="search">
+        <input type="text" placeholder="Search Product" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
