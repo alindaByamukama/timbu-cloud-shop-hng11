f19d75d36f631365a939dcc1d576eb713ab5fa57
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-08 15:12:58 +0300

    add file to store product data

---
+++ src/data/products.ts
@@ -0,0 +1,50 @@
+export const products = [
+    {
+        imageUrl: '/images/bamboo-lid.png',
+        name: 'Bamboo Lid',
+        description: 'Durable bottle with a sustainable bamboo lid.',
+        price: '₦20,000',
+    },
+    {
+        imageUrl: '/images/insulated.png',
+        name: '',
+        description: 'Keep your drinks hot or cold for hours.',
+        price: '',
+    },
+    {
+        imageUrl: '/images/sport.png',
+        name: '',
+        description: 'Perfect for workouts and outdoor activities.',
+        price: '',
+    },
+    {
+        imageUrl: '/images/straw-lid.png',
+        name: '',
+        description: 'Easy to drink straw lid, perfect for kids',
+        price: '',
+    },
+    {
+        imageUrl: '/images/wide-mouth.png',
+        name: '',
+        description: 'Wide mouth for easy filling and cleaning,',
+        price: '',
+    },
+    {
+        imageUrl: '/images/glass.png',
+        name: '',
+        description: 'Elegant glass bottle for everyday use.',
+        price: '',
+    },
+    {
+        imageUrl: '/images/collapsible.png',
+        name: '',
+        description: 'Space-saving collapsible cup for travel.',
+        price: '',
+    },
+    {
+        imageUrl: '/images/infuser.png',
+        name: '',
+        description: 'Classy infuser bottle for hydration.',
+        price: '',
+    },
+];
