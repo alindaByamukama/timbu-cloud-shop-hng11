8923d35e9fa359e9dddbe98379cdc216b0bf77af
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-08 15:47:50 +0300

    update hef styles

--- components/DescriptionLink.tsx
+++ components/DescriptionLink.tsx
@@ -9,7 +9,7 @@ const DescriptionLink: React.FC<DescriptionLinkProps> = ({ label, href }) => {
   return (
     <a
       href={href}
-      className="relative w-[119px] h-[32px] absolute left-[20px] top-[20px] font-poppins font-medium text-[20px] leading-[32px] text-[#777777] hover:text-[#D55D51] transition duration-300"
+      className="font-poppins font-medium text-[20px] leading-[32px] text-[#777777] hover:text-[#D55D51] transition duration-300"
     >
       {label}
     </a>
