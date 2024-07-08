ca4b2568c5c47f143031366307394fa736d2e1c3
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-08 12:58:44 +0300

    add footer component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import '../styles/globals.css';
 
 const Layout: React.FC = ({ children }) => {
@@ -7,6 +8,7 @@ const Layout: React.FC = ({ children }) => {
     <div>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 };
